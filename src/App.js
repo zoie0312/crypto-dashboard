aaa04690a768b35a551c1fb18563cc5240f89b29
Zoie <greenmoonshisu@gmail.com>
2022-06-16 15:21:31 +0800

    making sidebar

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './components/Sidebar';
+
+function App() {
+    return (
+        <div className="App">
+            
+            <div className="AppGlass">
+              <Sidebar/>
+              <div></div>
+              <div></div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,46 @@
+//import React from 'react';
+import React, { useState } from 'react'
+
+import './Sidebar.css';
+import Logo from '../imgs/logo.png';
+
+import { SidebarData } from '../Data/Data.js';
+import {UilSignOutAlt} from "@iconscout/react-unicons";
+
+const Sidebar = () => {
+    const [selected, setSelected] = useState(0);
+    
+    return (
+        <div className="Sidebar">
+            <div className="header">
+                <img src={Logo} alt="" />
+                <span>
+                    Cr<span>yp</span>to
+                </span>
+            </div>
+
+            <div className="menu">
+                {
+                    SidebarData.map((item, idx) => {
+                        return (
+                            <div 
+                                className={"menuItem" + (selected === idx ? " active" : "")} 
+                                key={idx}
+                                onClick={() => setSelected(idx)}>
+                                <item.icon/>
+                                <span>{item.heading}</span>
+                            </div>
+                        )
+                    })
+                }
+                <div className="menuItem">
+                    <UilSignOutAlt/>
+                    
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
